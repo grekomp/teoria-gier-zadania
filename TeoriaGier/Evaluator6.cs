20e124c18fec9a9b88d06d7433c313371fd03068
Grzegorz Palian <greekomp@gmail.com>
2018-01-28 18:46:29 +0100

    Initial Commit

---
+++ TeoriaGier/Evaluator6.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeoriaGier
+{
+    class Evaluator6
+    {
+        public float Evaluate(string game)
+        {
+
+
+            return 0;
+        }
+
+        public List<string> GetOptions(string game, char player)
+        {
+            List<string> options = new List<string>();
+
+            for (int i = 0; i < game.Length; i++)
+            {
+                if (game.ElementAt(i) == player)
+                {
+                    options.Add(RemoveNthUp(game, i));
+                }
+            }
+
+            return options;
+        }
+
+        public string RemoveNthUp(string game, int n)
+        {
+            StringBuilder newGameBuilder = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                newGameBuilder.Append("" + game.ElementAt(i));
+            }
+
+            return newGameBuilder.ToString();
+        }
+    }
+}
---
+++ TeoriaGier/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeoriaGier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string game = "Haba baba";
+
+            Evaluator6 evaluator6 = new Evaluator6();
+            Console.WriteLine(evaluator6.RemoveNthUp(game, 4));
+
+            Console.ReadLine();
+        }
+    }
+}
